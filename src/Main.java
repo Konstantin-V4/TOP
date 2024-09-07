7aae83daba0a40429efdc50a4da7ecc7389a5d72
Konstantin-V4 <konstantin-v4@mail.ru>
2024-09-07 15:56:01 +0500

    HomeWork (Lesson 7-8)

--- src/Main.java
+++ src/Main.java
@@ -40,54 +40,47 @@ public class Main {
         // Сканнер
 
         Scanner sc = new Scanner(System.in);
+        System.out.print("Введите сложение: ");
+        int cars = sc.nextInt();
+        System.out.print("Введите вычитание: ");
+        int buyers = sc.nextInt();
+        System.out.print("Введите умножение: ");
+        int target = sc.nextInt();
+        System.out.print("Введите деление: ");
+        int result = sc.nextInt();
+        System.out.print("Введите остаток от процента: ");
+        int deduction = sc.nextInt();
+
 
         int a = 50;
         int b = 25;
 
-        int cars = a + b;
-        int buyers = a - b;
-        int target = a * b;
-        int result = a / b;
-        int deduction = a % b;
+        //int cars = a + b;
+        //int buyers = a - b;
+        //int target = a * b;
+        //int result = a / b;
+        //int deduction = a % b;
 
-        System.out.print("Сложение: ");
-        cars = sc.nextInt();
-        System.out.print("Вычитание: ");
-        buyers = sc.nextInt();
-        System.out.print("Умножение: ");
-        target = sc.nextInt();
-        System.out.print("Деление: ");
-        result = sc.nextInt();
-        System.out.print("Остаток от деления: ");
-        deduction = sc.nextInt();
+        System.out.println("Сложение: " + cars);
+        System.out.println("Вычитание: " + buyers);
+        System.out.println("Умножение: " + target);
+        System.out.println("Деление: " + result);
+        System.out.println("Остаток от деления: " + deduction);
 
         // Использование класса Random
 
         Random winner = new Random();
+        int int1 = winner.nextInt(0,100);
+        int int2 = winner.nextInt(0,100);
+        int int3 = winner.nextInt(0,100);
+        int int4 = winner.nextInt(0,100);
 
-        char char1 = 'A';
-        System.out.print(char1);
-        int firstGroupOfParticipants = char1;
-        firstGroupOfParticipants = winner.nextInt(0, 100);
-        System.out.println(firstGroupOfParticipants);
+        char char1 = (char) int1;
+        char char2 = (char) int2;
+        char char3 = (char) int3;
+        char char4 = (char) int4;
 
-        char char2 = 'B';
-        System.out.print(char2);
-        int secondGroupOfParticipants = char2;
-        secondGroupOfParticipants = winner.nextInt(101,200);
-        System.out.println(secondGroupOfParticipants);
-
-        char char3 = 'C';
-        System.out.print(char3);
-        int thirdGroupOfParticipants = char3;
-        thirdGroupOfParticipants = winner.nextInt(201,300);
-        System.out.println(thirdGroupOfParticipants);
-
-        char char4 = 'D';
-        System.out.print(char4);
-        int fourthGroupOfParticipants = char3;
-        fourthGroupOfParticipants = winner.nextInt(301,400);
-        System.out.println(fourthGroupOfParticipants);
+        System.out.println("Пароль: " + char1 + char2 + char3 + char4);
 
         // Переполнение типа int
 
