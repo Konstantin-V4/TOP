9945789dcc7dab10769506a6aae7a27b6ab3d9f4
Konstantin-V4 <konstantin-v4@mail.ru>
2024-09-08 19:04:22 +0500

    HomeWork 9-10

--- src/HomeWorks/HW2.java
+++ src/HomeWorks/HW2.java
@@ -3,61 +3,61 @@ package HomeWorks;
 public class HW2 {
     public static void main(String[] args) {
 
-    // Начало
+        // Начало
 
-    int yes = 1;
-    int no = 0;
+        int yes = 1;
+        int no = 0;
 
-    if (yes > no) {
-        System.out.println("Поставили чайник");
-    } else if (yes < no) {
-        System.out.println("Не поставили чайник");
-    }
-
-        boolean stoveOn = true;
-        boolean takeCup = true;
-        boolean putCoffee = true;
-        boolean putSugar = true;
-
-        if (stoveOn) {
-            System.out.println("Включили плитку");
-        }
-        if (takeCup) {
-            System.out.println("Достали кружку");
-        }
-        if (putCoffee) {
-            System.out.println("Положили кофе в кружку");
-        }
-        if (putSugar) {
-            System.out.println("Положили сахар в кружку");
+        if (yes > no) {
+            System.out.println("Поставили чайник");
+        } else if (yes < no) {
+            System.out.println("Не поставили чайник");
         }
 
+            boolean stoveOn = true;
+            boolean takeCup = true;
+            boolean putCoffee = true;
+            boolean putSugar = true;
 
-    if (yes > no) {
-        System.out.println("Чайник вскипел");
-    } else if (yes < no) {
-        System.out.println("Чайник не вскипел");
-    }
+            if (stoveOn) {
+              System.out.println("Включили плитку");
+            }
+            if (takeCup) {
+              System.out.println("Достали кружку");
+            }
+            if (putCoffee) {
+                System.out.println("Положили кофе в кружку");
+            }
+            if (putSugar) {
+                System.out.println("Положили сахар в кружку");
+            }
 
 
-        boolean poorWater = true;
-        boolean stirCoffee = true;
-
-        if (poorWater) {
-            System.out.println("Налили воды в кружку");
-        }
-        if (stirCoffee) {
-            System.out.println("Размешали кофе");
+        if (yes > no) {
+            System.out.println("Чайник вскипел");
+        } else if (yes < no) {
+            System.out.println("Чайник не вскипел");
         }
 
 
-    if (yes > no) {
-        System.out.println("Кофе готово");
-    } else if (yes < no) {
-        System.out.println("Кофе не готово");
-    }
+            boolean poorWater = true;
+            boolean stirCoffee = true;
 
-    // Конец
+            if (poorWater) {
+                System.out.println("Налили воды в кружку");
+            }
+            if (stirCoffee) {
+                System.out.println("Размешали кофе");
+            }
+
+
+        if (yes > no) {
+           System.out.println("Кофе готово");
+        } else if (yes < no) {
+            System.out.println("Кофе не готово");
+        }
+
+        // Конец
 
     }
 }
